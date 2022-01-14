01c16c588eb4cf78221544af0b2bfcc9b4ac8991
AaronW <aaronwhildkhild@yahoo.com>
2022-01-13 22:24:04 -0600

    Finally

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,15 @@
 --Part 1
+Id = Int;
+Name = long;
+EmployerId = Int;
 
 --Part 2
+Select name From employers
+Where location = "St. Louis City";
 
 --Part 3
-
+SELECT *
+FROM skills
+INNER JOIN jobskills
+ON skills.Id = jobskills.SkillId
+ORDER BY skills.Name ASC;
